4c470d5b4cdb583f8d5ecfc8e4411da7791b5be0
Mr. Komal Kumar <suryavansi8650@gmail.com>
2026-02-24 16:48:25 +0400

    Update container.py

--- container.py
+++ container.py
@@ -5,16 +5,11 @@ git config --global http.proxy $http_proxy
 git config --global https.proxy $https_proxy
 git config --global http.proxyAuthMethod 'basic'
 
-$http_proxy
-http://svc_proxy_nonrte_netgrp:jLNgeTuuCdbu9h9kesPaA4iEDrzHvXtR@172.22.13.140:3128git config --global http.proxy "$http_proxy"
-git config --global https.proxy "$https_proxy"
-git config --global http.proxyAuthMethod 'basic'
-
 singularity shell -B ~/:/workspace 
-export HF_HOME=/vast/users/hisham.cholakkal/documents/cachepath/hf
-export MIOPEN_USER_DB_PATH="/vast/users/hisham.cholakkal/tmp_dir/miopen_node8"
+export HF_HOME=~/documents/cachepath/hf
+export MIOPEN_USER_DB_PATH="~/tmp_dir/miopen_node8"
 export MIOPEN_CUSTOM_CACHE_DIR=${MIOPEN_USER_DB_PATH}
 rm -rf ${MIOPEN_USER_DB_PATH}
 mkdir -p ${MIOPEN_USER_DB_PATH}
 
-cd /workspace/documents/rewardit/DanceGRPO/
+cd /workspace/documents
